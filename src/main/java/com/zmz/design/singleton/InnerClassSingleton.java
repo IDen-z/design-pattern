172be7581a803a4c93962760a3c1b1a894caa988
Secrets <zmzxdal@foxmail.com>
2022-07-14 23:48:09 +0800

    设计模式学习

--- src/main/java/com/zmz/design/singleton/InnerClassSingleton.java
+++ src/main/java/com/zmz/design/singleton/InnerClassSingleton.java
@@ -1,9 +1,14 @@
 package com.zmz.design.singleton;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
  * 利用静态内部类的方法 实现懒加载
  */
-public class InnerClassSingleton {
+public class InnerClassSingleton implements Serializable {
+
+    private static final long serialVersionUID = 444203445677988857L;
 
     // 饿汉式的单例模式会利用jvm的类加载保证单例
     // 懒汉式除了经典的双检锁模式外 还可利用静态内部类的方式实现
@@ -21,7 +26,7 @@ public class InnerClassSingleton {
 
     // 私有化构造函数
     private InnerClassSingleton() {
-        if (InnerClassSingletonHolder.instance!=null){
+        if (InnerClassSingletonHolder.instance != null) {
             throw new RuntimeException("不允许初始化多个实例");
         }
     }
@@ -31,5 +36,8 @@ public class InnerClassSingleton {
         return InnerClassSingletonHolder.instance;
     }
 
+    public Object readResolve() throws ObjectStreamException {
+        return InnerClassSingletonHolder.instance;
+    }
 
 }
--- src/main/java/com/zmz/design/singleton/TestReflect.java
+++ src/main/java/com/zmz/design/singleton/TestReflect.java
@@ -1,32 +1,46 @@
 package com.zmz.design.singleton;
 
+import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class TestReflect {
 
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        String name = InnerClassSingleton.class.getName();
-        System.err.println(name);
-        Constructor<InnerClassSingleton> declaredConstructor = InnerClassSingleton.class.getDeclaredConstructor();
-        declaredConstructor.setAccessible(true);
-        InnerClassSingleton innerClassSingleton = declaredConstructor.newInstance();
-        InnerClassSingleton instance = InnerClassSingleton.getInstance();
-        // 普通的单例模式 可以利用反射进行多实例的初始化
-        // 如果采用静态内部类或者饿汉式时 可以在私有构造器中加以判断防止多实例
-        // 懒汉式 (双检锁模式) 暂时无法防止反射
-        System.err.println(innerClassSingleton);
-        System.err.println(instance);
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, IOException, ClassNotFoundException {
+//        String name = InnerClassSingleton.class.getName();
+//        System.err.println(name);
+//        Constructor<InnerClassSingleton> declaredConstructor = InnerClassSingleton.class.getDeclaredConstructor();
+//        declaredConstructor.setAccessible(true);
+//        InnerClassSingleton innerClassSingleton = declaredConstructor.newInstance();
+//        InnerClassSingleton instance = InnerClassSingleton.getInstance();
+//        // 普通的单例模式 可以利用反射进行多实例的初始化
+//        // 如果采用静态内部类或者饿汉式时 可以在私有构造器中加以判断防止多实例
+//        // 懒汉式 (双检锁模式) 暂时无法防止反射
+//        System.err.println(innerClassSingleton);
+//        System.err.println(instance);
+//
+//        // 测试双检锁
+//        // 发现测试不通过
+//        // 因为懒汉式 双检锁模式 是需要用的时候才去构造
+//        Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor(String.class);
+//        constructor.setAccessible(true);
+//        Singleton singleton = constructor.newInstance("1");
+//        Singleton singleton2 = Singleton.getInstance("1");
+//        System.err.println(singleton);
+//        System.err.println(singleton2);
 
-        // 测试双检锁
-        // 发现测试不通过
-        // 因为懒汉式 双检锁模式 是需要用的时候才去构造
-        Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor(String.class);
-        constructor.setAccessible(true);
-        Singleton singleton = constructor.newInstance("1");
-        Singleton singleton2 = Singleton.getInstance("1");
-        System.err.println(singleton);
-        System.err.println(singleton2);
+        // 还有一点值得注意的 可以通过序列化和反序列化的方式破坏单例模式
+        // 利用readResolve()
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("testSeri"));
+        InnerClassSingleton instance2 = InnerClassSingleton.getInstance();
+        oos.writeObject(instance2);
+        oos.close();
+        System.err.println(instance2);
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("testSeri"));
+        InnerClassSingleton innerClassSingleton2 = (InnerClassSingleton) ois.readObject();
+        ois.close();
+        System.err.println(innerClassSingleton2);
 
 
     }
